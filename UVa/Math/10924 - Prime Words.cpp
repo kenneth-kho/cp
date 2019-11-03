3e39651bad5a8865e6128cc28c49f5e63464a2bc
kenneth-kho <51808510+kenneth-kho@users.noreply.github.com>
2019-11-03 21:40:29 +0700

    Create 10924 - Prime Words.cpp

---
+++ UVa/Math/10924 - Prime Words.cpp
@@ -0,0 +1,14 @@
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1865
+#include<iostream>
+using namespace std;
+int main(){
+	string s;
+	while(getline(cin,s)){
+		int prime=1,total=0;
+		for(int i=0;s[i]!='\0';i++){
+			if(s[i]>='a'&&s[i]<='z')total+=s[i]-'a'+1;
+			else total+=s[i]-'A'+27;
+		}for(int i=2;i<total;i++)if(total%i==0)prime=0;
+		cout<<(prime?"It is a prime word.\n":"It is not a prime word.\n");
+	}
+}
