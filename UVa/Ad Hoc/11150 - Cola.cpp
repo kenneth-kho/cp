ad656e09d9f378409e1df84d603f8a86288acb76
kenneth-kho <51808510+kenneth-kho@users.noreply.github.com>
2019-11-03 19:44:09 +0700

    Create 11150 - Cola.cpp

---
+++ UVa/Ad Hoc/11150 - Cola.cpp
@@ -0,0 +1,9 @@
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2091
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	while(cin>>n){
+		cout<<n*3/2<<'\n';
+	}
+}
